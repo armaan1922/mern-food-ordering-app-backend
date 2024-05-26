cbbbd27052d6925711ef2122fab618eef78ebbed
armaan1922 <armaans.gehri@gmail.com>
2024-05-26 14:35:40 +0530

    added checkout flow

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import myUserRoute from "./routes/myUserRoute"
 import {v2 as cloudinary} from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
+import OrderRoute from "./routes/OrderRoute"
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=> console.log("Connected to databse !"));
 
 
@@ -16,8 +17,11 @@ cloudinary.config({
   });
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*"}));
+
+app.use(express.json());
 
 app.get("/health", async (req: Request, res: Response) => {
 
@@ -26,7 +30,8 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
-app.use("/api/restaurant", restaurantRoute)
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", OrderRoute);
 
 app.listen(7000, ()=>{
     console.log("server started at localhost:7000");
--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -4,7 +4,16 @@ import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router();
 
-
+router.get(
+    "/:restaurantId",
+    param("restaurantId")
+      .isString()
+      .trim()
+      .notEmpty()
+      .withMessage("RestaurantId paramenter must be a valid string"),
+    RestaurantController.getRestaurant
+  );
+  
 router.get("/search/:city", param("city").isString().trim().notEmpty().withMessage("city parameter must be a valid string"), RestaurantController.searchRestaurant);
 
 export default router;
